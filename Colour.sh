61c3ac331904fa82b248cc8546cf7d5d4289f6fa
JogendraGhodke <jogendraghodke14@gmail.com>
2024-07-12 00:15:50 +0530

    changes

--- Colour.sh
+++ Colour.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-for c in 90 31 91 32 33 34 35 95 36 97; do
+for c in 90 31 91 32 33 34 35 95 36 97 30 37 92 93 94 95 96 97; do
 		echo -en "\r \e[${c}m JOGENDRA \e[0m "
-		sleep 10
+		sleep 1
 
 done
